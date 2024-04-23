2345fdc153578d98df30c8f4cf53d5d15fae9382
Serge-35 <166376730+Serge-35@users.noreply.github.com>
2024-04-23 08:28:55 +0300

    Домашняя работа по уроку "Пространство имен и способы вызова функции" - доработан

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-def test_1(a=10, b=100):
+def test_1(a, b):
     print(a, b)
 
 def test_2(param1, param2, param3):
     print(param1, param2, param3)
 
-test_1()
+test_1(10, 20)
 test_2('I learn', 'Python', 'modul3')
 
