84ee56f7bb8b33c1537afaf46519f9cee7fb8784
Serge-35 <166376730+Serge-35@users.noreply.github.com>
2024-04-25 10:34:23 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-def test_1(a, b):
+def test_1():
+    a, b = 3, 5
     print(a, b)
 
 def test_2(param1, param2, param3):
     print(param1, param2, param3)
 
-test_1(10, 20)
+test_1()
 test_2('I learn', 'Python', 'modul3')
 
