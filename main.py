e915b38f5ca1faefd688660dd4f052a0b2e5d88d
Serge-35 <166376730+Serge-35@users.noreply.github.com>
2024-05-17 19:43:20 +0300

    Домашнее задание по уроку "Модули и пакеты"

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def test_1():
-    a, b = 3, 5
-    print(a, b)
+from mod4les1 import *
 
-def test_2(param1, param2, param3):
-    print(param1, param2, param3)
-
-test_1()
-test_2('I learn', 'Python', 'modul3')
+hi_all()
 
